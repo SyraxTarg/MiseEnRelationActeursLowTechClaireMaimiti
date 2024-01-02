bde02e8e56eb5a0fe4b9489d1ae7a6311f1dc221
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-01-02 22:05:34 +0100

    css page projet

--- views/pages/home.php
+++ views/pages/home.php
@@ -1,41 +1,57 @@
-<img src="./public/images/presTinyHouse.jpg" alt="Visuel sur une tiny house de Low Tech">
+<div id="home">
+    <img class="home_main_img" src="./public/images/presTinyHouse.jpg" alt="Visuel sur une tiny house de Low Tech">
 
-<section id="projet">
-    <div class="icon_right">
-        <h2>Le Projet</h2>
-        <img src=".\public\images\elements\elements3_hammer.png" alt="Icone marteau">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam?</p>
-    </div>
-</section>
+    <section id="projet" class="section_with_icon">
+        <div class="icon_right">
+            <div class="home_text">
+                <h2 class="home_section_title">Le projet</h2>
+                <p class="home_description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus aliquid debitis repudiandae libero, eligendi quibusdam? Eius omnis est delectus ut, quo dignissimos provident quisquam non sint. Odio vero doloribus consequuntur. Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, tempora dolor minus dignissimos soluta mollitia nemo quis, voluptate vero aliquam voluptatibus corporis tenetur consequatur dolorum voluptatem ullam, dolores quod in.</p>
+            </div>
+            <img class="home_icon" src=".\public\images\elements\elements3_hammer.png" alt="Icone marteau">
+        </div>
+    </section>
 
-<section id="objectifs">
-    <div class="icon_left">
-        <h2>Les objectifs</h2>
-        <img src=".\public\images\elements\elements5_recycle.png" alt="Icone tri selectif">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam?</p>
-    </div>
-</section>
+    <section id="objectifs" class="section_with_icon">
+        <div class="icon_left">
+            <div class="home_text">
+                <h2 class="home_section_title">Les objectifs</h2>
+                <p class="home_description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam? Lorem ipsum, dolor sit amet consectetur adipisicing elit. Ex unde, minima beatae corporis, cum libero magnam natus doloribus quod placeat non sunt? Ad dignissimos porro nemo repellendus, expedita modi ducimus! Lorem ipsum dolor sit, amet consectetur adipisicing elit. Modi sequi minus hic praesentium quaerat dolore totam debitis explicabo suscipit? Neque fuga perspiciatis ducimus repellendus doloribus aliquid cumque, laborum tempora voluptas.</p>
+            </div>
+            <img class="home_icon" src=".\public\images\elements\elements5_recycle.png" alt="Icone tri selectif">
+        </div>
+    </section>
 
-<section id="acteurs">
-    <div class="icon_right">
-        <h2>Les acteurs</h2>
-        <img src=".\public\images\elements\elements6_earth.png" alt="Icone Terre">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam?</p>
-    </div>
+    <section id="acteurs">
+        <div>
+            <div class="section_with_icon">
+                <div class="icon_right">
+                    <div class="home_text">
+                        <h2 class="home_section_title">Les acteurs</h2>
+                        <p class="home_description">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Fuga vitae dolorum nesciunt. Sunt amet dolorem, magnam odit repellendus quasi, doloribus dolores perferendis fugit itaque consequuntur nam consequatur pariatur sequi sed? Lorem ipsum dolor sit, amet consectetur adipisicing elit. Reiciendis temporibus ea sed, a praesentium autem architecto natus quidem aliquam fugit laudantium quisquam aut sapiente ipsam non similique ab eius. Ab!</p>
+                    </div>
+                    <img class="home_icon" src=".\public\images\elements\elements6_earth.png" alt="Icone Terre">
+                </div>
+            </div>
 
-    <div class="vert_clair_bg">
-        <img src=".\public\images\tiny_lab_logo.png" alt="Logo de Tiny Lab">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam?</p>
+            <div class="vert_clair_bg">
+                <div>
+                    <img class="actor_logo" id="tiny_lab_logo" src=".\public\images\tiny_lab_logo.png" alt="Logo de Tiny Lab">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Odit officiis ut quaerat incidunt magnam perspiciatis labore similique, rerum id dignissimos fugit omnis, at voluptate repellendus odio earum, illum fugiat maiores?</p>
+                </div>
 
-        <img src=".\public\images\la_planche_logo.png" alt="Logo de La Planche">
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam?</p>
-    </div>
-</section>
+                <div>
+                    <img class="actor_logo" id="la_planche_logo" src=".\public\images\la_planche_logo.png" alt="Logo de La Planche">
+                    <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam? Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque cumque tempora facilis optio doloribus explicabo rem nulla? Eum consectetur molestias recusandae quas, error, sapiente ut, eos beatae nemo assumenda consequatur.</p>
+                </div>
+            </div>
+        </div>
+    </section>
 
-<section id="enjeux">
-    <div class="vert_moyen_bg">
-        <h2>Les enjeux</h2>
-        <h3>Titre 2 exemple : </h3>
-        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam?</p>
-    </div>
-</section>
+    <section id="enjeux">
+        <div class="vert_moyen_bg">
+            <h2 class="home_section_title">Les enjeux</h2>
+            <h3>Titre 3 exemple : </h3>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Officiis perspiciatis delectus, dignissimos quo, voluptas alias enim voluptatum, reiciendis optio nulla soluta ullam sunt debitis. Quis, laboriosam asperiores. Assumenda, commodi vero? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Molestias, fugiat veniam. Vitae architecto necessitatibus non repellat aspernatur aliquam, repudiandae possimus qui dolorem? Soluta quaerat deserunt labore eaque ipsa esse laboriosam? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Provident, natus culpa. Odio repudiandae eos nam aliquid omnis ea totam quod, sunt suscipit, assumenda velit rem nobis id. Molestiae, nulla illo!</p>
+        </div>
+    </section>
+</div>
