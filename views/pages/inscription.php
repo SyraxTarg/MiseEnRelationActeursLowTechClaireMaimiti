1e8e70224c50d5933856ea1fba2180b9634c3a0e
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-01-03 19:19:32 +0100

    css connexion inscription modif profil

--- views/pages/inscription.php
+++ views/pages/inscription.php
@@ -1,32 +1,46 @@
-<a href="index.php?page=home">Retour à l'accueil</a>
+<div class="user">
 
-<h1>Inscription</h1>
+    <h1>Inscription</h1>
 
-<p>Les informations que vous entrez seront visibles publiquement sur la plateforme.</p>
+    <p>Les informations que vous entrez seront visibles publiquement sur la plateforme.</p>
 
-<?php
-if(isset($msg))
-    echo $msg;
-?>
+    <?php
+    if (isset($msg))
+        echo $msg;
+    ?>
 
-<form method="POST">
-    <label for="username">Username </label>
-    <input type="text" name="username" id="username" required>
+    <form method="POST" class="user_form">
+        <div class="user_form_group">
+            <label for="username">Username </label>
+            <input type="text" name="username" id="username" required>
+        </div>
 
-    <label for="email">Email </label>
-    <input type="email" name="email" id="email" required>
+        <div class="user_form_group">
+            <label for="email">Email </label>
+            <input type="email" name="email" id="email" required>
+        </div>
 
-    <label for="password">Mot de passe </label>
-    <input type="password" name="password" id="password" required>
-    <label for="reEnterPassword">Entrez à nouveau votre mot de passe </label>
-    <input type="password" name="reEnterPassword" id="reEnterPassword" required>
+        <div class="user_form_group">
+            <label for="password">Mot de passe </label>
+            <input type="password" name="password" id="password" required>
+        </div>
 
-    <label for="activites">Activités </label>
-    <input type="text" name="activites" id="activites">
+        <div class="user_form_group">
+            <label for="reEnterPassword">Entrez à nouveau votre mot de passe </label>
+            <input type="password" name="reEnterPassword" id="reEnterPassword" required>
+        </div>
 
-    <input type="submit" value="Continuer">
-</form>
+        <div class="user_form_group">
+            <label for="activites">Activités </label>
+            <input type="text" name="activites" id="activites">
+        </div>
 
-<br>
-<span>Vous avez déjà un compte ? </span>
-<a href="index.php?page=connexion">Connexion</a>
+        <input class="user_submit_button" type="submit" value="Continuer">
+    </form>
+
+    <br>
+    <div class="row_align">
+        <span>Vous avez déjà un compte ?</span>
+        <a class="link_action" href="index.php?page=connexion">Connexion</a>
+    </div>
+</div>
