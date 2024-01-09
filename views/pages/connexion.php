39c37d6729d306a519fc235b92745ec3f4b9835f
Claire Sbaffe <claire.sbaffe@efrei.net>
2024-01-09 13:59:34 +0100

    revert responsive connexion

--- views/pages/connexion.php
+++ views/pages/connexion.php
@@ -19,18 +19,6 @@
         <input class="user_submit_button" type="submit" value="Me connecter">
     </form>
 
-    <!-- <form method="POST" class="user_form" id="connexion_form">
-        <div class="user_form_group">
-            <label for="email">Email </label>
-            <input type="email" name="email" id="email" required>
-        </div>
-        <div class="user_form_group">
-            <label for="password">Mot de passe </label>
-            <input type="password" name="password" id="password" required>
-        </div>
-        <input class="user_submit_button" type="submit" value="Me connecter">
-    </form> -->
-
     <div class="row_align">
         <span>Pas encore de compte ? </span>
         <a class="link_action" href="index.php?page=inscription">Inscription</a>
